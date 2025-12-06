a5a3e324af11bb92721d28253131c67c34546752
languint <longuint@proton.me>
2025-12-05 22:19:44 -0600

    ci(www): add Dockerfile

--- ftc-emu-www/src/App.tsx
+++ ftc-emu-www/src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import init, { greet } from "../pkg/ftc_emu_core";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    async function loadWasm() {
+      await init();
+      greet("ftc-emu-www");
+    }
+    loadWasm();
+  }, []);
+
   return <></>;
 }
 
