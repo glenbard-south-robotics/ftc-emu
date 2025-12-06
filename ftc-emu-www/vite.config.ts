f57506a0617f4803cb0301a1b7c60c082dd56e2e
languint <longuint@proton.me>
2025-12-06 16:01:20 -0600

    chore: initialize shadcn for ftc-emu-www

--- ftc-emu-www/vite.config.ts
+++ ftc-emu-www/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 5173,
@@ -12,6 +14,11 @@ export default defineConfig({
       interval: 100,
     },
     strictPort: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   optimizeDeps: {
     exclude: ["ftc-emu-core"],
